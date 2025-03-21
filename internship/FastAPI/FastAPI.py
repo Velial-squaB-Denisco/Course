70b27034b68e2a7d20b75e0f1e8bd6ff138f3855
Velial-squaB-Denisco <ppoi42477@gmail.com>
2025-03-21 14:42:48 +0300

    v1.3

--- internship/FastAPI/FastAPI.py
+++ internship/FastAPI/FastAPI.py
@@ -1,7 +1,8 @@
-import uvicorn 
+import uvicorn
+from fastapi import FastAPI
 
 def main():
-    pass
+    uvicorn.run("FastAPI:app", host = '0.0.0.0', port = 8000, reload = True, workers = 3) 
 
 if __name__ == '__main__':
     main()
