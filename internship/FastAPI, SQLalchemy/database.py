dd99ff02ed36ad68012dcf37401852f9fb79f33e
Velial-squaB-Denisco <ppoi42477@gmail.com>
2025-03-21 15:03:49 +0300

    v1.7

--- internship/FastAPI, SQLalchemy/database.py
+++ internship/FastAPI, SQLalchemy/database.py
@@ -3,3 +3,17 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_URL = "sqllite:///./sql_app.db"
+
+engine = create_engine(SQLALCHEMY_URL, create_engine = {"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit = False ,autoflush = False, bind = engine)
+Base = declarative_base() 
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+
+    finally:
+        db.close()
+        
