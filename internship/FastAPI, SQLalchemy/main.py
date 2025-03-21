ea3643c734b87b63eb0b536f2f4657c6c0e37d6c
Velial-squaB-Denisco <ppoi42477@gmail.com>
2025-03-21 16:10:43 +0300

    v1.16

--- internship/FastAPI, SQLalchemy/main.py
+++ internship/FastAPI, SQLalchemy/main.py
@@ -4,7 +4,6 @@ from database import SessionLocal, engine, Base
 from routers import user as UserRouter
 
 Base.metadata.create_all(bild = engine)
-
 app = FastAPI()
 app.include_router(UserRouter.router, prefix = "/user")
 
