ee1305b73b7ef96b1356f3c9c9c4690a94a249bb
Velial-squaB-Denisco <ppoi42477@gmail.com>
2025-03-21 15:21:19 +0300

    v1.8

--- internship/FastAPI, SQLalchemy/FastAPI.py
+++ internship/FastAPI, SQLalchemy/FastAPI.py
@@ -1,7 +1,10 @@
 import uvicorn
 from fastapi import FastAPI
+from database import SessionLocal, engine, Base
+
+Base.metadata.create_all(bild = engine)
 
 app = FastAPI()
 
 if __name__ == '__main__':
-    uvicorn.run("FastAPI:app", host = '0.0.0.0', port = 8000, reload = True, workers = 3) 
+    uvicorn.run("FastAPI:app", host = '0.0.0.0', port = 8000, reload = True, workers = 3)
