9a4444b542a40151c3f9f8dfff6d5d30dea0b8ac
Velial-squaB-Denisco <ppoi42477@gmail.com>
2025-03-24 17:28:30 +0300

    v2.17

--- internship/FastAPI_SQLalchemy/main.py
+++ internship/FastAPI_SQLalchemy/main.py
@@ -2,7 +2,7 @@
 import sqlalchemy
 import uvicorn
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
@@ -23,9 +23,13 @@ books = [
 def read_books():
     return books
 
-@app.get("/books/id")
-def book():
-    pass
+@app.get("/books/{book_id}")
+def get_book(book_id: int):
+    for book in books:
+        if books["id"] == book_id:
+            return book
+        
+        raise HTTPException(status_code=404)
 
 if __name__ == '__main__':
     uvicorn.run("main:app", reload=True)
