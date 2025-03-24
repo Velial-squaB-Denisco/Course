d92e05a3f46a40282316af8771b57120fa90ecb2
Velial-squaB-Denisco <ppoi42477@gmail.com>
2025-03-24 13:48:53 +0300

    v2.2

--- internship/FastAPI, SQLalchemy alf.1/main.py
+++ internship/FastAPI, SQLalchemy alf.1/main.py
@@ -8,4 +8,4 @@ app = FastAPI()
 app.include_router(UserRouter.router, prefix = "/user")
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host = '0.0.0.0', port = 8080, reload = True, workers = 3)
+    uvicorn.run("main:app", host = 'localhost', port = 8000, reload = True, workers = 3)
