6ccf181a43b2fd5b73d81e225f7565d88299094e
Velial-squaB-Denisco <ppoi42477@gmail.com>
2025-03-24 22:08:41 +0300

    t1

--- internship/FastAPI_SQLalchemy/main.py
+++ internship/FastAPI_SQLalchemy/main.py
@@ -1,62 +1,47 @@
 #uvicorn main:app --reload
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
-from sqlalchemy.orm import sessionmaker, Session
-
 import uvicorn
-from fastapi import FastAPI, HTTPException, Depends
-
 from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
-metadata = MetaData()
-engine = create_engine('sqlite:///books.db')
 
-#//////////////////////////////////////////////////////////////////////////////
-
-books = Table('books', metadata,
-              Column('id', Integer, primary_key=True),
-              Column('title', String),
-              Column('author', String))
-
-metadata.create_all(engine)
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-#//////////////////////////////////////////////////////////////////////////////
+books = [
+    {
+        "id": 1,
+        "title": "Асинхронность в Python",
+        "author": "Маттью",
+    },
+    {
+        "id": 2,
+        "title": "Python",
+        "author": "Митч",
+    }
+]
 
 @app.get("/books")
-def read_books(db: Session = Depends(get_db)):
-    return db.execute(books.select()).fetchall()
+def read_books():
+    return books
 
 @app.get("/books/{book_id}")
-def get_book(book_id: int, db: Session = Depends(get_db)):
-    query = books.select().where(books.c.id == book_id)
-    book = db.execute(query).fetchone()
-    if book is None:
+def get_book(book_id: int):
+    for book in books:
+        if book["id"] == book_id:
+            return book
+        
         raise HTTPException(status_code=404, detail="Book not found")
-    return book
-
+    
 class NewBook(BaseModel):
     title: str
     author: str
 
 @app.post("/books")
-def create_book(new_book: NewBook, db: Session = Depends(get_db)):
-    new_book_data = {
+def create_books(new_book: NewBook):
+    books.append({
         "id": len(books) + 1,
         "title": new_book.title,
         "author": new_book.author
-    }
-    db.execute(books.insert().values(**new_book_data))
-    db.commit()
-    return new_book_data
+    })
+
 
 if __name__ == '__main__':
     uvicorn.run("main:app", reload=True)
