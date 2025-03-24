6a19316dbf19e76496404e57b1a55e23f142502f
Velial-squaB-Denisco <ppoi42477@gmail.com>
2025-03-24 17:09:15 +0300

    v2.14

--- internship/FastAPI_SQLalchemy/main.py
+++ internship/FastAPI_SQLalchemy/main.py
@@ -6,9 +6,22 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+books = [
+    {
+        "id": 1,
+        "title": "Асинхронность в Python",
+        "author": "Маттью",
+    },
+    {
+        "id": 2,
+        "title": "Python",
+        "author": "Митч",
+    }
+]
+
 @app.get("/", summary="Main", tags=["Root"])
 def Home():
     return "Hello world"
 
 if __name__ == '__main__':
-    uvicorn.run("main:app")
+    uvicorn.run("main:app", reload=True)
