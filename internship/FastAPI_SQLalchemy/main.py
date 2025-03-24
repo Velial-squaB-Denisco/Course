95a14357855cdbf25e16256574802c59620a6014
Velial-squaB-Denisco <ppoi42477@gmail.com>
2025-03-24 16:26:21 +0300

    v2.11

--- internship/FastAPI_SQLalchemy/main.py
+++ internship/FastAPI_SQLalchemy/main.py
@@ -1,5 +1,12 @@
-from fastapi import FastAPI
 import sqlalchemy
+
+from fastapi import FastAPI
 
 app = FastAPI()
 
+def root():
+    return "hello"
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
