4be9d928a9cb3eda17711e34fd549a67e626d7c1
Velial-squaB-Denisco <ppoi42477@gmail.com>
2025-03-24 18:46:14 +0300

    v2.23

--- internship/FastAPI_SQLalchemy/main.py
+++ internship/FastAPI_SQLalchemy/main.py
@@ -1,6 +1,6 @@
 #uvicorn main:app --reload
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
 
 import uvicorn
 from fastapi import FastAPI, HTTPException, Depends
@@ -9,13 +9,19 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+# Создание модуля для метаданных
 metadata = MetaData()
-engine = create_engine('sqlite:///books.db') 
+
+# Инициализация базы данных
+engine = create_engine('sqlite:///books.db')
+
+# Определение таблицы "books"
 books = Table('books', metadata,
               Column('id', Integer, primary_key=True),
               Column('title', String),
               Column('author', String))
 
+# Создание таблиц в базе данных
 metadata.create_all(engine)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -28,24 +34,25 @@ def get_db():
         db.close()
 
 @app.get("/books")
-def read_books():
-    return books
+def read_books(db: Session = Depends(get_db)):
+    return db.execute(books.select()).fetchall()
 
 @app.get("/books/{book_id}")
-def get_book(book_id: int):
-    for book in books:
-        if book["id"] == book_id:
-            return book
-        
+def get_book(book_id: int, db: Session = Depends(get_db)):
+    query = books.select().where(books.c.id == book_id)
+    book = db.execute(query).fetchone()
+    if book is None:
         raise HTTPException(status_code=404, detail="Book not found")
-    
+    return book
+
 class NewBook(BaseModel):
     title: str
     author: str
 
 @app.post("/books")
-def create_book(new_book: NewBook, db=Depends(get_db)):
+def create_book(new_book: NewBook, db: Session = Depends(get_db)):
     new_book_data = {
+        "id": len(books) + 1,
         "title": new_book.title,
         "author": new_book.author
     }
