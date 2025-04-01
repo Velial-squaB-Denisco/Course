ac09340499f0b5df1325e235171eacef881cb384
Velial-squaB-Denisco <ppoi42477@gmail.com>
2025-04-01 17:39:43 +0300

    v4.6

--- internship/PyQT5/project/main.py
+++ internship/PyQT5/project/main.py
@@ -24,24 +24,45 @@ class MyWindow(QMainWindow):
         main_layout.addWidget(self.label)
 
         # /// Горизонтальный макет для кнопок
-        button_layout = QHBoxLayout()
+        button_layoutH = QHBoxLayout()
+        button_layoutV = QVBoxLayout()
 
-        # /// Start Button
+        # /// Start
         self.btnStart = QtWidgets.QPushButton("Start", self)
         self.btnStart.clicked.connect(self.start)
-        button_layout.addWidget(self.btnStart)
+        button_layoutH.addWidget(self.btnStart)
 
-        # /// Pause Button
-        self.btnPause = QtWidgets.QPushButton("Stop", self)
-        self.btnPause.clicked.connect(self.stop)
-        button_layout.addWidget(self.btnPause)
+        # /// Stop
+        self.btnStop = QtWidgets.QPushButton("Stop", self)
+        self.btnStop.clicked.connect(self.stop)
+        button_layoutH.addWidget(self.btnStop)
 
-        main_layout.addLayout(button_layout)
+        # /// Reset
+        self.btnReset = QtWidgets.QPushButton("Reset", self)
+        self.btnReset.clicked.connect(self.reset)
+        button_layoutV.addWidget(self.btnReset)
+
+        # /// Step
+        self.btnStep1 = QtWidgets.QPushButton("Step1", self)
+        self.btnStep1.clicked.connect(lambda:self.run_cmd(1))
+        button_layoutV.addWidget(self.btnStep1)
+
+        self.btnStep2 = QtWidgets.QPushButton("Step2", self)
+        self.btnStep2.clicked.connect(lambda:self.run_cmd(2))
+        button_layoutV.addWidget(self.btnStep2)
+
+        self.btnStep3 = QtWidgets.QPushButton("Step3", self)
+        self.btnStep3.clicked.connect(lambda:self.run_cmd(3))
+        button_layoutV.addWidget(self.btnStep3)
+
+        main_layout.addLayout(button_layoutV)
+        main_layout.addLayout(button_layoutH)
 
         # /// Output
         self.Output = QtWidgets.QTextEdit(self)
         self.Output.setReadOnly(True)
         main_layout.addWidget(self.Output)
+
 
         self.process = None
         self.thread = None
@@ -49,18 +70,38 @@ class MyWindow(QMainWindow):
 
         self.update_signal.connect(self.append_text)
 
+    def reset(self):
+        self.btnReset.setStyleSheet("background-color: gray;")
+        self.btnStart.setStyleSheet("background-color: None;")
+        self.btnStop.setStyleSheet("background-color: None;")
+
+        self.append_text("Reset")
+
     def start(self):
+        self.btnStart.setStyleSheet("background-color: green;")
+        self.btnReset.setStyleSheet("background-color: None;")
+        self.btnStop.setStyleSheet("background-color: None;")
+
+        self.append_text("Start")
+
         if not self.running:
             self.running = True
-            self.thread = threading.Thread(target=self.run_cmd)
-            self.thread.start()
+            for i in range(3):
+                self.thread = threading.Thread(target=self.run_cmd, args = (i + 1,))
+                self.thread.start()
 
     def stop(self):
+        self.btnStart.setStyleSheet("background-color: None;")
+        self.btnReset.setStyleSheet("background-color: None;")
+        self.btnStop.setStyleSheet("background-color: red;")
+
+        self.append_text("Stop")
+
         if self.running and self.process:
             self.running = False
             self.process.terminate()
 
-    def run_cmd(self):
+    def run_cmd(self, i):
 
         if getattr(sys, 'frozen', False):
             exe_dir = os.path.dirname(sys.executable)
@@ -68,7 +109,7 @@ class MyWindow(QMainWindow):
         else:
             base_dir = os.path.dirname(os.path.abspath(__file__))
 
-        batch_file_path = os.path.join(base_dir, 'cmd.bat')
+        batch_file_path = os.path.join(base_dir, f'cmd{i}.bat')
 
         self.process = subprocess.Popen(
             [batch_file_path],
