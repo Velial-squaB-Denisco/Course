d86bd8241b733601fdc79f80cd757de4ea9016ab
Velial-squaB-Denisco <ppoi42477@gmail.com>
2025-04-03 15:18:09 +0300

    v4.15

--- internship/PyQT5/project/main.py
+++ internship/PyQT5/project/main.py
@@ -3,9 +3,35 @@ import sys
 import platform
 import subprocess
 from threading import Thread
-from multiprocessing import Process
 from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QHBoxLayout, QWidget
+
+class Step:
+    def __init__(self, step_number):
+        self.step_number = step_number
+
+    def run_script(self):
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+
+        scripts_dir = os.path.join(base_dir, 'scripts')
+        if platform.system().lower() == "windows":
+            script_path = os.path.join(scripts_dir, f'cmd{self.step_number}.bat')
+        else:
+            script_path = os.path.join(scripts_dir, f'cmd{self.step_number}.sh')
+
+        return script_path
+
+class Step1(Step):
+    def __init__(self):
+        super().__init__(1)
+
+class Step2(Step):
+    def __init__(self):
+        super().__init__(2)
+
+class Step3(Step):
+    def __init__(self):
+        super().__init__(3)
 
 class MyWindow(QMainWindow):
     update_signal = QtCore.pyqtSignal(str)
@@ -57,15 +83,15 @@ class MyWindow(QMainWindow):
 
         # /// Step
         self.btnStep1 = QtWidgets.QPushButton("Step1", self)
-        self.btnStep1.clicked.connect(lambda: self.start_cmd(1))
+        self.btnStep1.clicked.connect(lambda: self.start_cmd(Step1()))
         button_layoutV.addWidget(self.btnStep1)
 
         self.btnStep2 = QtWidgets.QPushButton("Step2", self)
-        self.btnStep2.clicked.connect(lambda: self.start_cmd(2))
+        self.btnStep2.clicked.connect(lambda: self.start_cmd(Step2()))
         button_layoutV.addWidget(self.btnStep2)
 
         self.btnStep3 = QtWidgets.QPushButton("Step3", self)
-        self.btnStep3.clicked.connect(lambda: self.start_cmd(3))
+        self.btnStep3.clicked.connect(lambda: self.start_cmd(Step3()))
         button_layoutV.addWidget(self.btnStep3)
 
         main_layout.addLayout(button_layoutV)
@@ -80,8 +106,7 @@ class MyWindow(QMainWindow):
         self.Output.setReadOnly(True)
         main_layout.addWidget(self.Output)
 
-        self.system = platform.system().lower()
-        self.append_text(self.system)
+        self.append_text(platform.system().lower())
 
         self.progress_updated.connect(self.update_progress)
         self.reset_progress.connect(self.reset_progress_bar)
@@ -97,15 +122,10 @@ class MyWindow(QMainWindow):
         self.progressBar.setValue(value)
 
     def reset(self):
-        self.btnReset.setStyleSheet("background-color: gray;")
-        self.btnStart.setStyleSheet("background-color: None;")
-        self.btnStop.setStyleSheet("background-color: None;")
-
         if self.running and self.process:
             self.running = False
             self.process.wait()
             self.process.terminate()
-
 
         self.Output.clear()
         self.progressBar.setValue(0)
@@ -114,70 +134,59 @@ class MyWindow(QMainWindow):
             btn.setEnabled(True)
             btn.setStyleSheet("background-color: None;")
 
-
     def start(self):
-
         if self.running and self.process:
             self.running = False
             self.process.terminate()
             self.append_text("Stop")
 
-        self.running = True
-        self.btnStart.setStyleSheet("background-color: green;")
-        self.btnReset.setStyleSheet("background-color: None;")
-        self.btnStop.setStyleSheet("background-color: None;")
-        self.append_text("Start")
+        self.btnStart.setEnabled(False)
+        self.btnReset.setEnabled(False)
+        self.btnStep1.setEnabled(False)
+        self.btnStep2.setEnabled(False)
+        self.btnStep3.setEnabled(False)
 
+        self.running = True
+        self.append_text("Start")
         self.thread = Thread(target=self.run_cmds_sequentially)
         self.thread.start()
 
     def run_cmds_sequentially(self):
-        for i in range(3):
+        for step_class in [Step1, Step2, Step3]:
             if not self.running:
                 break
-            self.run_cmd(i + 1)
+            self.run_cmd(step_class())
 
-    def start_cmd(self, i):
-
+    def start_cmd(self, step_instance):
         if self.running and self.process:
             self.running = False
             self.process.terminate()
             self.append_text("Stop")
 
         self.running = True
-        self.thread = Thread(target=self.run_cmd, args=(i,))
+        self.thread = Thread(target=self.run_cmd, args=(step_instance,))
         self.thread.start()
 
     def stop(self):
-        self.btnStart.setStyleSheet("background-color: None;")
-        self.btnReset.setStyleSheet("background-color: None;")
-        self.btnStop.setStyleSheet("background-color: red;")
         self.append_text("Stop")
-
         if self.running and self.process:
             self.running = False
             self.process.terminate()
 
-    def run_cmd(self, i):
-        self.reset_progress.emit()
+        self.btnStart.setEnabled(True)
+        self.btnReset.setEnabled(True)
+        self.btnStep1.setEnabled(True)
+        self.btnStep2.setEnabled(True)
+        self.btnStep3.setEnabled(True)
 
-        if getattr(sys, 'frozen', False):
-            exe_dir = os.path.dirname(sys.executable)
-            base_dir = os.path.abspath(os.path.join(exe_dir, '..'))
-        else:
-            base_dir = os.path.dirname(os.path.abspath(__file__))
-            
-        scripts_dir = os.path.join(base_dir, 'scripts')
-        if self.system == "windows":
-            batch_file_path = os.path.join(scripts_dir, f'cmd{i}.bat')
-        else:
-            batch_file_path = os.path.join(scripts_dir, f'cmd{i}.sh')
-        
-        self.append_text(batch_file_path)
-        getattr(self, f'btnStep{i}').setStyleSheet("background-color: yellow;")
+    def run_cmd(self, step_instance):
+        self.reset_progress.emit()
+        script_path = step_instance.run_script()
+        self.append_text(script_path)
+        getattr(self, f'btnStep{step_instance.step_number}').setStyleSheet("background-color: yellow;")
 
         self.process = subprocess.Popen(
-            [batch_file_path],
+            [script_path],
             shell=True,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
@@ -190,9 +199,9 @@ class MyWindow(QMainWindow):
         self.read_output(self.process.stderr, "stderr")
 
         if self.running and self.process:
-            getattr(self, f'btnStep{i}').setStyleSheet("background-color: green;")
+            getattr(self, f'btnStep{step_instance.step_number}').setStyleSheet("background-color: green;")
         else:
-            getattr(self, f'btnStep{i}').setStyleSheet("background-color: red;")
+            getattr(self, f'btnStep{step_instance.step_number}').setStyleSheet("background-color: red;")
 
     def read_output(self, stream, stream_type):
         for line in iter(stream.readline, ""):
